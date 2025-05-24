7cc8c9b6d31ae9b97f7626b20681667b0535a633
Rian Septiawan <rianseptiawan@infitech.or.id>
2025-05-24 23:14:12 +0700

    re

--- frontend/src/types/phaser.d.ts
+++
@@ -1,5 +0,0 @@
-// This file tells TypeScript that the 'phaser' module has its own type definitions
-declare module 'phaser' {
-  // Phaser has its own internal TypeScript definitions
-  export * from 'phaser/types/phaser';
-}
